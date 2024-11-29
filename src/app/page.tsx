6b22884952db795bb3a6454dc59146e2249405e6
Itachi-Uchiha581 <adeebfaiyaz@gmail.com>
2024-11-29 12:53:02 +0530

    Fixed the type error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,8 +24,12 @@ export default function Sentimenter() {
         const data = await analyze(url);
         setSentimentScore(data?.sentiment.document.score);
         setSentimentLabel(data?.sentiment.document.label);
-      } catch (err: any) {
-        setError(err.message || 'An error occurred.');
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError('An error occurred.');
+        }
       }
     });
   };
